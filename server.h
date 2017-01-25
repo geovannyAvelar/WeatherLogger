be128493a108a7c58fa1580dda5e5ffc25ec4d34
Geovanny Avelar <geovanny.avelar@gmail.com>
2017-01-25 21:11:34 -0200

    Server address

--- server.h
+++ server.h
@@ -2,7 +2,7 @@
 #include <Ethernet.h>
 #include <ArduinoHttpClient.h>
 
-#define SERVER_IP "10.0.0.100"
+#define SERVER_IP "138.197.76.251"
 #define SERVER_PORT 8080
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //MAC address DE:AD:BE:EF:FE:ED
